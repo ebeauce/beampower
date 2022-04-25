cb3b1059b6f6bad375297b7b112fd3bb73383424
Eric Beauce <ebeauce@mit.edu>
2022-04-25 12:29:32 -0400

    Add keyword argument for using phasenet or not

--- scripts/4_compute_cnr_NAF.py
+++ scripts/4_compute_cnr_NAF.py
@@ -10,8 +10,6 @@ import utils
 import read_asdf
 import beamnetresponse as bnr
 
-#from phasenet import wrapper as PN
-
 try:
     from scipy.stats import median_abs_deviation as scimad
 except ImportError:
@@ -26,10 +24,18 @@ path_data = os.path.join(path_script, os.pardir, 'data')
 # I/O variables
 tt_filename = 'tts.h5'
 data_filename = 'data_20120726.h5'
-output_filename = 'cnr_NAF_env.h5'
 
 # computation architecture
 device = 'cpu'
+
+# use PhaseNet in the example or not (default = use envelopes)
+use_phasenet = False
+
+if use_phasenet:
+    from phasenet import wrapper as PN
+    output_filename = 'cnr_NAF_PN.h5'
+else:
+    output_filename = 'cnr_NAF_env.h5'
 
 # ------------------------------------------------------
 #    load travel-times, network metadata and waveforms
@@ -60,33 +66,37 @@ moveouts = utils.sec_to_samp(moveouts, sr=sr)
 # ------------------------------------------------------
 #         define the detection traces
 # ------------------------------------------------------
-# -- use envelopes as detection traces
-norm = scimad(data_arr, axis=-1)[..., np.newaxis]
-norm[norm == 0.] = 1.
-print(norm[0, 0, 0])
-detection_traces = utils.envelope_parallel(data_arr/norm)
-# clip envelopes so that spikes don't dominate the beam
-scale = scimad(detection_traces, axis=-1)[..., np.newaxis]
-detection_traces = np.clip(detection_traces, np.min(detection_traces, axis=-1, keepdims=True),
-                           10.**5.*scale)
-# -- use PhaseNet as detection traces
-#PN_probas, _ = PN.automatic_picking(
-#        data_arr[np.newaxis, ...], metadata['station_code'], '.',
-#        'test', mini_batch_size=256)
-#detection_traces = np.swapaxes(PN_probas.squeeze(), 2, 1)
+if use_phasenet:
+    # -- use PhaseNet as detection traces
+    PN_probas, _ = PN.automatic_picking(
+            data_arr[np.newaxis, ...], metadata['station_code'], '.',
+            'test', mini_batch_size=256)
+    detection_traces = np.swapaxes(PN_probas.squeeze(), 2, 1)
+else:
+    # -- use envelopes as detection traces
+    norm = scimad(data_arr, axis=-1)[..., np.newaxis]
+    norm[norm == 0.] = 1.
+    detection_traces = utils.envelope_parallel(data_arr/norm)
+    # clip envelopes so that spikes don't dominate the beam
+    scale = scimad(detection_traces, axis=-1)[..., np.newaxis]
+    detection_traces = np.clip(
+            detection_traces, np.min(detection_traces, axis=-1, keepdims=True),
+            10.**5.*scale)
 
 # ------------------------------------------------------
 #         define the weight matrices
 # ------------------------------------------------------
 # -- weights_phases: used to pre-stack the channels for each phase
-# ---- weights for envelope detection traces
-weights_phases = np.ones(detection_traces.shape[:-1] + (len(phases),), dtype=np.float32)
-weights_phases[:, :2, 0] = 0. # P-wave weight = 0 for horizontal components
-weights_phases[:, 2, 1] = 0. # S-wave weight = 0 for vertical components
-# ---- weights for PhaseNet detection traces
-#weights_phases = np.ones((detection_traces.shape[:-1]) + (2,), dtype=np.float32)
-#weights_phases[:, 0, 1] = 0. # S-wave weights to zero for channel 0
-#weights_phases[:, 1, 0] = 0. # P-wave weights to zero for channel 1
+if use_phasenet:
+    # ---- weights for PhaseNet detection traces
+    weights_phases = np.ones((detection_traces.shape[:-1]) + (2,), dtype=np.float32)
+    weights_phases[:, 0, 1] = 0. # S-wave weights to zero for channel 0
+    weights_phases[:, 1, 0] = 0. # P-wave weights to zero for channel 1
+else:
+    # ---- weights for envelope detection traces
+    weights_phases = np.ones(detection_traces.shape[:-1] + (len(phases),), dtype=np.float32)
+    weights_phases[:, :2, 0] = 0. # P-wave weight = 0 for horizontal components
+    weights_phases[:, 2, 1] = 0. # S-wave weight = 0 for vertical components
 
 # -- weights_sources: source-specific weights, for example to limit
 #                     the contribution of remote stations
